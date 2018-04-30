da5c42ea7c392ae5c0cd3da6b46657d90e636eea
Andri Danusasmita <andridsasmita@gmail.com>
2018-04-30 13:41:42 +0900

    Added separated XML labels and PNG datasets into

--- get_png_xml.py
+++ get_png_xml.py
@@ -1,23 +1,34 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+"""
+Sample Usage:
+from project root:
+`python get_png_xml.py`
+"""
 
 import os
 import shutil
 
 # copy only pngs that have corresponding xml
-def get_xml(src_dir="png/", dst_dir="png_f/"):
+def get_xml(src_dir="data/all_img", img_dst_dir="data/img", xml_dst_dir="data/xml"):
     """
-    find PNGs in src_dir that are labeled with XMLS,
-    then copy the files to dst_dir
+    find PNGs in src_dir that are labeled with XML files,
+    then copy the PNG files to img_dst_dir, and
+    copy the XML files to xml_dst_dir
     
     Args:
-        src_dir (str): path to files
-        dst_dir (str): path to destination directory
+        src_dir: path to files
+        img_dst_dir: path to image destination
+        xml_dst_dir: path to xml destination
     """
     cdict = {}
-    if not os.path.exists(dst_dir):
-        os.makedirs(dst_dir)
-        print(f"created directory: {dst_dir}")
+    if not os.path.exists(img_dst_dir):
+        os.makedirs(img_dst_dir)
+        print(f"created directory: {img_dst_dir}")
+
+    if not os.path.exists(xml_dst_dir):
+        os.makedirs(xml_dst_dir)
+        print(f"created directory: {xml_dst_dir}")
     
     for keyword in os.listdir(src_dir):
         print(keyword)
@@ -26,15 +37,16 @@ def get_xml(src_dir="png/", dst_dir="png_f/"):
         for f in os.listdir(path):
             if os.path.splitext(f)[1] == '.xml':
                 count += 1
-                for ext in ['.xml','.png']:
-                    src_path = os.path.join(path,os.path.splitext(f)[0]+ext)
-                    shutil.copy2(src_path,dst_dir)
-
+                img_src_path = os.path.join(path,os.path.splitext(f)[0]+'.png')
+                shutil.copy2(img_src_path,img_dst_dir)
+                xml_src_path = os.path.join(path,os.path.splitext(f)[0]+'.xml')
+                shutil.copy2(xml_src_path,xml_dst_dir)
         cdict[keyword] = count
+        
     print("done")
-    for k,v in dict1.items():
+    for k,v in cdict.items():
         print(f"xml count for {k}:{v}")
 
 if __name__ == "__main__":
-    get_xml(src_dir="png/", dst_dir="png_f/")
+    get_xml(src_dir="data/all_img", img_dst_dir="data/img", xml_dst_dir="data/xml")
 
